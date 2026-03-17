f0116c1a9cf114551092f8686c02275a4e38a2b9
Benjamin Dehli <superelg@gmail.com>
2026-03-17 17:03:10 +0100

    Expose utility functions and classes in the main entry point
    Centralize exports from data, element, text resource, and validation modules to provide a single point of access for the library's public API.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import CustomElementHtmlAttributes from "./classes/CustomElementHtmlAttributes";
-
 export { CustomElementHtmlAttributes };
+
+export { getDataForComponent, hasValue, getValueFromDataKey } from "./scripts/dataHelpers.js";
+export { addContainerElement, appendChildren, addStyle, calculateFlexWidth, createCustomElement } from "./scripts/elementHelpers.js";
+export {
+    getTextResources,
+    getDefaultTextResources,
+    getTextResourceFromResourceBinding,
+    getTextResourcesFromResourceBindings
+} from "./scripts/textResourcesHelpers.js";
+export { isValidHeaderSize, isValidTagName } from "./scripts/validators.js";
