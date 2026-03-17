0ef739bbb3373d0d2bf728f8d83f4d05e4738ab9
Benjamin Dehli <superelg@gmail.com>
2026-03-17 14:38:47 +0100

    Add tsup

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/index.js"],
+    format: ["esm", "cjs"],
+    dts: true,
+    splitting: true,
+    sourcemap: true,
+    clean: true,
+    treeshake: true,
+    minify: true
+});
