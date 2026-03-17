164d2d6acaf74a2074478ea486000deea72d6387
Benjamin Dehli <superelg@gmail.com>
2026-03-17 14:54:03 +0100

    Add typescript and update tsup config
    Add typescript as a dev dependency and disable declaration file generation in the tsup configuration.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     entry: ["src/index.js"],
     format: ["esm", "cjs"],
-    dts: true,
     splitting: true,
     sourcemap: true,
     clean: true,
