1208b04bc339f6c9868e3f4208ac393acd4995ae
Benjamin Dehli <superelg@gmail.com>
2026-03-25 09:12:28 +0100

    Update elementHelpers documentation for non-element children
    Clarify that non-HTMLElement children are converted to text nodes and appended via appendChild rather than using innerHTML.

--- src/scripts/elementHelpers.js
+++ src/scripts/elementHelpers.js
@@ -4,8 +4,8 @@ import { isValidTagName } from "./validators";
 
 /**
  * Appends an array of children to a parent element. If a child is an instance of HTMLElement,
- * it is appended using `appendChild`. Otherwise, the child's content is appended to the parent's
- * innerHTML.
+ * it is appended using `appendChild`. Otherwise, the child is converted to a text node and
+ * appended to the parent using `appendChild`.
  *
  * @param {HTMLElement} parent - The parent element to which the children will be appended.
  * @param {Array<HTMLElement|string>} children - An array of children to append. Each child can be
