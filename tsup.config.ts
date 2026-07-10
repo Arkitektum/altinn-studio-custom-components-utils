bfe14a04d1092bf7274ecf1a6bf36265b023d632
Benjamin Dehli <benjamin@arkitektum.no>
2026-07-10 16:48:26 +0200

    Add .d.ts generation

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     entry: ["src/index.js"],
     format: ["esm", "cjs"],
+    dts: true,
     splitting: true,
     sourcemap: true,
     clean: true,
